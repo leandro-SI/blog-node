1bf5a647cf8982cbd35b028e4f0fb832a0f851f6
leandro-si <leandro.almeida@uvvnet.com.br>
2024-07-04 10:36:33 -0300

    formulario de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -2,12 +2,9 @@ const express = require('express')
 const router = express.Router();
 
 
-router.get('/categories', (req, res) => {
-    res.send("ROTA DE CATEGORIAS")
-});
-
+// Rotas de Categories
 router.get('/admin/categories/new', (req, res) => {
-    res.send("ROTA PARA CRIAR UMA NOVA CATEGORIA")
+    res.render('admin/categories/new')
 })
 
 module.exports = router;
