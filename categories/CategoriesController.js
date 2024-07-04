9f03dc26ed411abeb604bd13b02f3759cbb52afe
leandro-si <leandro.almeida@uvvnet.com.br>
2024-07-04 11:01:18 -0300

    cadastro de categorias

--- categories/CategoriesController.js
+++ categories/CategoriesController.js
@@ -1,10 +1,26 @@
 const express = require('express')
 const router = express.Router();
+const slugify = require('slugify');
+const Category = require('./Category');
+
 
 
 // Rotas de Categories
 router.get('/admin/categories/new', (req, res) => {
     res.render('admin/categories/new')
+})
+
+router.post('/categories/save', (req, res) => {
+    
+    let data = {
+        title: req.body.title,
+        slug: slugify(req.body.title)
+    }
+
+    Category.create(data)
+        .then((result) => {
+            res.redirect('/')
+        })
 })
 
 module.exports = router;
