49bfa726681ccaf90f824ba3b06e4904c6ddca52
leandro-si <leandro.almeida@uvvnet.com.br>
2024-07-05 00:09:47 -0300

    salvando artigos

--- articles/ArticlesController.js
+++ articles/ArticlesController.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const Category = require('../categories/Category');
+const Article = require('./Article');
+const slugify = require('slugify');
 
 
-router.get('/articles', (req, res) => {
+router.get('/admin/articles', (req, res) => {
     res.send("ROTA DE ARTIGOS")
 });
 
@@ -13,6 +15,20 @@ router.get('/admin/articles/new', (req, res) => {
         res.render("admin/articles/new", { categories: result })
     })
 
+})
+
+router.post('/articles/save', function (req, res) {
+  
+    let data = {
+        title: req.body.title,
+        body: req.body.body,
+        categoryId: req.body.category,
+        slug: slugify(req.body.title)
+    }
+
+    Article.create(data).then((result) => {
+        res.redirect('/admin/articles')
+    })
 })
 
 module.exports = router;
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize');
 
 const connection = new Sequelize('guia-blog', 'root', 'root', {
     host: 'localhost',
-    dialect: 'mysql'
+    dialect: 'mysql',
+    timezone: '-03:00'
 });
 
 module.exports = connection;
